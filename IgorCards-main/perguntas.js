024ce3f3b294d4a32c46778ba4277137b5520386
Leonardo-26-2A <00001098387612sp@al.educacao.sp.gov.br>
2024-11-28 20:30:28 -0300

    Add files via upload

---
+++ IgorCards-main/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação amplamente usada em aplicações da Web, desenvolvimento de software, ciência de dados e machine learning (ML). Os desenvolvedores usam o Python porque é eficiente e fácil de aprender e pode ser executada em muitas plataformas diferentes.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é a ONU?',
+    'A Organização das Nações Unidas (ONU) é uma organização internacional fundada em 1945 para intermediar as relações internacionais, harmonizar a ação das nações diante de objetivos comuns, atuar para o desenvolvimento mundial e garantir a paz.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'JavaScript é uma poderosa linguagem de programação que pode adicionar interatividade a um site. Foi inventado por Brendan Eich. JavaScript é versátil e amigável para iniciantes. Com mais experiência, você poderá criar jogos, gráficos 2D e 3D animados, aplicativos abrangentes baseados em banco de dados.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Bom em Inglês?',
+    'Oi em ingles é Good! (ɡo͝od)'
+)
